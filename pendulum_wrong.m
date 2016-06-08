220f9d8bd01f5f4e611f730812969b9d644daca2
Garrett Wilson <g@floft.net>
2016-06-07 22:43:49 -0700

    pendulum_wrong is tuned for rd=rd^2, which is wrong

---
+++ pendulum_wrong.m
@@ -0,0 +1,469 @@
+%
+% Control the inverted pendulum
+%
+%close all;
+clear all;
+pkg load control;
+
+% Import UFIR functions
+ufir;
+
+% Whether to create plots
+plotAll = false;
+
+%
+% Pendulum model, the longer rod
+%
+
+% Sample rate / control frequency (Hz)
+f = 200;
+T = 1/f;
+Maxpos = 0.25;              % Max carriage travel +- 0.25 m
+Maxangle = 0.175;           % Max rod angle -- 10 deg
+Maxvoltage = 20;            % Max motor voltage, V
+pstart = 0.005;             % Carriage position starting limit, m
+astart = 1*pi/180;          % Angle starting limit, rad
+g = 9.81;                   % m/s^2     Gravitational constant
+
+% SYSTEM PARAMETERS
+% Measured Mechanical Parameters
+d1 = 0.323;                % m         Length of pendulum 1 (long)
+d2 = 0.079;                 % m         Length of pendulum 2 (short)
+%mp1 = 0.0208;              % kg        Mass of pendulum 1
+mp1 = 0.0318;
+%mp2 = 0.0050;              % kg        Mass of pendulum 2
+mp2 = 0.0085;
+%m=0.8; % Makes state estimates better for some reason
+m = 0.3163;                 % kg        Mass of carriage
+rd = 0.0254/2;              % m         Drive pulley radius
+md = 0.0375;                % kg        Mass of drive pulley (cylinder)
+%mc1 = 0.0036;              % kg        Mass of clamp 1*
+%mc2 = 0.0036;              % kg        Mass of clamp 2*
+mc1 = 0.0085;
+mc2 = mc1;
+
+% *Clamp Dimensions
+%  Rectangular 0.0254 x 0.01143 m
+%  The pivot shaft is 0.00714 m from the end
+
+% Motor Parameters (Data Sheet)
+Im = 43e-7;                 % kg m^2/rad    Rotor moment of inertia
+R = 4.09;                   % ohms          Resistance
+kt = 0.0351;                % Nm/A          Torque constant
+ke = 0.0351;                % Vs/rad        Back emf constant
+
+% Derived Mechanical Parameters
+
+                            % kg m^2/rad    Moment of inertia, clamp 1
+%Ic1 = mc1*(0.01143^2 + 0.0254^2)/12 + mc1*(0.0127-0.00714)^2;
+Ic1 = mc1*(0.0098^2 + 0.0379^2)/12;
+Ic2 = Ic1;                  % kg m^2/rad    Moment of inertia, clamp 2
+Id = md*(rd^2)/2;           % kg m^2/rad    Moment of inertia, drive pulley
+Imd = Im + Id;              % kg m^2/rad    Moment of inertia, combined
+
+J1 = Ic1 + mp1*(d1^2)/3;    % Total moment of inertia, pendulum 1 (long)
+J2 = Ic2 + mp2*(d2^2)/3;    % Total moment of inertia, pendulum 2 (short)
+Jd = Im + Id;               % Total moment of inertia, motor drive
+Mc = m + mc1 + mc2;         % Total carriage mass
+
+% Friction Test Data
+%   Carriage Slope = 19 deg;  Terminal Velocity xdotss = 0.312 m/s; From
+%       twincarriage.m; formula b = m g sin(theta)/xdotss
+%   Pendulum 1 (long) Exponent a1 = 0.0756 1/s;  From longfit.m
+%   Pendulum 2 (short) Exponent a2 = 0.2922 1/s; From shortfit.m
+%        formula b = 2 a J
+
+%alpha = 19;
+alpha = 12.2;
+%xdotss = 0.312;
+xdotss = 0.4852;
+%a1 = 0.0756;
+%a2 = 0.2922;
+a1 = 0.0185;
+a2 = 0.012;
+                            % Ns/m    Viscous friction of carriage system
+b = (Mc + mp1 + mp2)*g*sin(alpha*pi/180)/xdotss;
+b1 = 2*a1*J1;               % Nms/rad    Viscous friction of pendulum 1 (rotational)
+b2 = 2*a2*J2;               % Nms/rad    Viscous friction of pendulum 2 (rotational)
+
+scale = [rd*2*pi/4096  2*pi/4096 -0.05/250];
+
+%
+% TF from cart position to PWM value
+%
+s = tf('s');
+inductance = 0; % H, negligible
+H = (kt/rd)*(s*Jd+b1)/(s^2*Jd*inductance + s*(Jd*R+b1*inductance) + (b1*R+kt^2));
+sysmotor = ss(H);
+A1 = sysmotor.a;
+B1 = sysmotor.b;
+C1 = sysmotor.c;
+D1 = sysmotor.d;
+
+%
+% System modeling
+%
+% http://ctms.engin.umich.edu/CTMS/index.php?example=InvertedPendulum&section=SystemModeling
+%
+M = Mc;                     % Mass of cart
+m = mp1;                    % Mass of pendulum 1
+b = b;                      % Friction
+I = J1;                     % Inertia of pendulum
+g = 9.81;                   % m/s^2     Gravitational constant
+l = d1/2;                   % Length of pendulum
+K = kt;                     % Motor constants
+r = rd;                     % Radius of motor
+R = R;                      % Resistance
+
+p = I*(M+m)+M*m*l^2; %denominator for the A and B matrices
+
+A2 = [0      1              0           0;
+     0 -(I+m*l^2)*b/p  (m^2*g*l^2)/p   0;
+     0      0              0           1;
+     0 -(m*l*b)/p       m*g*l*(M+m)/p  0];
+B2 = [     0;
+     (I+m*l^2)/p;
+          0;
+        m*l/p];
+C2 = [1 0 0 0;
+     0 0 1 0];
+D2 = [0;
+     0];
+
+%states = {'x' 'x_dot' 'phi' 'phi_dot'};
+%inputs = {'u'};
+%outputs = {'x'; 'phi'};
+%sys_ss = ss(A,B,C,D,'statename',states,'inputname',inputs,'outputname',outputs);
+
+%A = [A1 zeros(2,4); [B1*C1; zeros(2,2)] A2];
+%B = [B1; B2*D1];
+%C = [D2*C1 C2];
+%D = D2*D1;
+
+% Combining motor and pendulum equations
+p = (M+m)*(I+m*l^2)/(m*l)-m*l; %denominator for the A and B matrices
+A = [0  1                                     0                            0;
+     0  (I+m*l^2)/(m*l)*(-(b+K^2/(R*r^2))/p)  (I+m*l^2)/(m*l)*(M+m)*g/p-g  0;
+     0  0                                     0                            1;
+     0  -(b+K^2/(R*r^2))/p                    (M+m)*g/p                    0];
+B = [0;
+     (I+m*l^2)/(m*l)*(K/(R*r)/p);
+     0;
+     K/(R*r)/p];
+C = [1 0 0 0;
+     0 0 1 0];
+D = [0;
+     0];
+
+
+states = {'x' 'x_dot' 'phi' 'phi_dot'};
+inputs = {'v'};
+outputs = {'x'; 'phi'};
+sys_ss = ss(A,B,C,D,'statename',states,'inputname',inputs,'outputname',outputs);
+%sys_ss = ss(A2,B2,C2,D2,'statename',states,'inputname',inputs,'outputname',outputs);
+
+% Poles for our system
+original_poles = eig(A);
+
+%
+% Discretize
+%
+sys_d = c2d(sys_ss, T, 'zoh');
+%H = tf(sys_d);
+
+% Check if controllable and observable
+co = ctrb(sys_d);
+ob = obsv(sys_d);
+
+controllability = rank(co);
+observability = rank(ob);
+
+if controllability == 4
+	disp('System is controllable. Yay!')
+else
+	disp('System is not controllable. This is bad.')
+end
+if observability == 4
+	disp('System is observable. Yay!')
+else
+	disp('System is not observable. This is bad.')
+end
+
+% Original poles are:
+%
+%   0.00000 + 0.00000i
+%  -4.44367 + 0.00000i
+%  -0.04895 + 5.15645i
+%  -0.04895 - 5.15645i
+%
+%
+% With V -> x,phi model:
+%
+%    0.00000 + 0.00000i
+%   -9.77403 + 0.00000i
+%   -0.04054 + 5.12175i
+%   -0.04054 - 5.12175i
+%
+% which are way out of the unit circle.
+%
+poles = eig(A);
+
+%
+% Create a controller with LQR and simulate it
+% http://ctms.engin.umich.edu/CTMS/index.php?example=InvertedPendulum&section=ControlStateSpace
+%
+
+% Try 1, cart flies away and the settling time is high (from example)
+Q = C'*C;
+R = 1;
+
+% Try 2, cart doesn't fly away but the settling time is still high (from example)
+Q = C'*C;
+Q(1,1) = 5000;
+Q(3,3) = 100;
+R = 1;
+
+% Try 3, better
+Q = C'*C;
+%Q(1,1) = 1e9;
+%Q(3,3) = 1e9;
+%Q(1,1) = 2000;
+%Q(3,3) = 200;
+%Q(1,1) = 10000;
+%Q(3,3) = 200;
+%Q(1,1) = 48000;
+%Q(3,3) = 100;
+Q(1,1) = 3000;
+Q(3,3) = 100000;
+R = 0.1;
+
+K = lqr(A,B,Q,R);
+
+Ac = [(A-B*K)];
+Bc = [B];
+Cc = [C];
+Dc = [D];
+
+sys_cl = ss(Ac,Bc,Cc,Dc,'statename',states,'inputname',inputs,'outputname',outputs);
+
+t = 0:0.01:4;
+r = 0.2*ones(size(t));
+if plotAll
+    figure;
+    [y,t,x]=lsim(sys_cl,r,t);
+    [AX,H1,H2] = plotyy(t,y(:,1),t,y(:,2),'plot');
+    set(get(AX(1),'Ylabel'),'String','cart position (m)');
+    set(get(AX(2),'Ylabel'),'String','pendulum angle (radians)');
+    title('4th Order Step Response with LQR Control');
+    print -dpng '4th_Order_Step_Response_with_LQR_Control.png'
+end
+
+%
+% Correcting the cart position error. Now the cart actually ends up at 0.2
+% meters as we were commanding it.
+%
+Cn = [1 0 0 0];
+sys_ss = ss(A,B,Cn,0);
+Nbar = rscale(sys_ss,K);
+
+sys_cl = ss(Ac,Bc*Nbar,Cc,Dc,'statename',states,'inputname',inputs,'outputname',outputs);
+
+r =0.2*ones(size(t));
+if plotAll
+    figure;
+    [y,t,x]=lsim(sys_cl,r,t);
+    [AX,H1,H2] = plotyy(t,y(:,1),t,y(:,2),'plot');
+    set(get(AX(1),'Ylabel'),'String','cart position (m)');
+    set(get(AX(2),'Ylabel'),'String','pendulum angle (radians)');
+    title('4th Order Step Response with Precompensation and LQR Control');
+    print -dpng '4th_Order_Step_Response_with_Precompensation_and_LQR_Control.png'
+end
+
+%
+% Designing observer/estimator
+%
+% Our slowest pole's real value is at -1.7891. Let's try placing our
+% estimator's poles at -20.
+%
+%  -10.0491 + 10.4058i
+%  -10.0491 - 10.4058i
+%   -1.7891 +  3.5523i
+%   -1.7891 -  3.5523
+%
+% With new model:
+%
+%    -11.8992 + 10.2206i
+%    -11.8992 - 10.2206i
+%    -1.5386  +  4.2382i
+%    -1.5386  -  4.2382i
+%
+poles = eig(Ac);
+
+%P = [-50 -51 -52 -53];
+P = [-30 -31 -32 -33];
+L = place(A',C',P)';
+
+%
+%
+%
+Ace = [(A-B*K) (B*K);
+       zeros(size(A)) (A-L*C)];
+Bce = [B*Nbar;
+       zeros(size(B))];
+Cce = [Cc zeros(size(Cc))];
+Dce = [0;0];
+
+states_est = {'x' 'x_dot' 'phi' 'phi_dot' 'e1' 'e2' 'e3' 'e4'};
+sys_est_cl = ss(Ace,Bce,Cce,Dce,'statename',states_est,'inputname',inputs,'outputname',outputs);
+
+r = 0.2*ones(size(t));
+if plotAll
+    figure;
+    [y,t,x]=lsim(sys_est_cl,r,t);
+    [AX,H1,H2] = plotyy(t,y(:,1),t,y(:,2),'plot');
+    set(get(AX(1),'Ylabel'),'String','cart position (m)');
+    set(get(AX(2),'Ylabel'),'String','pendulum angle (radians)');
+    title('4th Order Step Response with Observer-Based State-Feedback Control');
+    print -dpng '4th_Order_Step_Response_with_Observer-Based_State-Feedback_Control.png'
+end
+
+%
+% Performing the simulation without using lsim
+%
+sys_est_d = c2d(sys_est_cl, T, 'zoh');
+
+% This is slow and not really needed... just a proof of concept
+if plotAll
+%if false
+    % State variables, the current and past values
+    state = zeros(size(A,1)*2, 2);
+
+    % The states we want to plot
+    N = 4*f;
+    output = zeros(N, size(C,1));
+
+    % Constant input command of zero
+    r = 0.2
+    input = r*ones(N);
+
+    for i = 1:N
+        state(:,1) = sys_est_d.a*state(:,2) + sys_est_d.b*input(i);
+
+        % Save for plot
+        output(i,:) = sys_est_d.c*state(:,2) + sys_est_d.d*input(i);
+
+        % New values are now the old values
+        state(:,2) = state(:,1);
+    end
+
+    figure;
+    plot(output);
+    t = 0:T:(size(output,1)-1)/f;
+    [AX,H1,H2] = plotyy(t,output(:,1),t,output(:,2),'plot');
+    set(get(AX(1),'Ylabel'),'String','cart position (m)');
+    set(get(AX(2),'Ylabel'),'String','pendulum angle (radians)');
+    title('4th Order All States Measured - Discrete');
+    print -dpng '4th_Order_All_States_Measured_Discrete.png'
+end
+
+%
+% Simulating the plant separately from our control and esimation, getting
+% closer to what will actually run on the pendulum
+%
+
+% To get discretized
+sys_est_only = ss(A, [B L], C, [D [0;0] [0;0]]);
+sys_est_only_d = c2d(sys_est_only, T, 'zoh');
+
+if plotAll
+%if false
+    % We have 4 state variables, and need the current and past values
+    state = zeros(size(A,1), 2);
+    eststate = zeros(size(A,1), 2);
+
+    % The states we want to plot
+    N = 4*f;
+    output = zeros(N, size(C,1));
+    estoutput = zeros(size(C,1), 1);
+    estoutputhistory = zeros(N, size(C,1));
+    firoutputhistory = zeros(N, size(A,1));
+
+    % Constant input command
+    r = 0.2;
+    input = zeros(N,1);
+
+    % UFIR filter init
+    UFIR_N = 30;
+    Y = zeros(size(C,1),UFIR_N);
+
+    for i = 1:N
+        % Our control law
+        input(i) = r*Nbar - K*eststate(:,1);
+        %input(i) = r*Nbar - K*(eststate(:,1) - [1;0;0;0]*r);
+        %input(i) = r*Nbar - K*[state(1,1); eststate(2,1); state(3,1); eststate(4,1)];
+
+        % Max out at +/- Maxvoltage
+        input(i) = min(max(input(i),-Maxvoltage),Maxvoltage);
+
+        % Estimation
+        y = [state(1,1); state(3,1)]; % Use only the measured part of the state
+        y = y+diag([1e-3 1e-6])*stdnormal_rnd(size(y)); % Add Gaussian noise to our measurements
+        uvec = [input(i); y-estoutput];
+        eststate(:,1) = sys_est_only_d.a*eststate(:,2) + sys_est_only_d.b*uvec;
+        estoutput = sys_est_only_d.c*eststate(:,2) + sys_est_only_d.d*uvec;
+
+        % UFIR Estimation
+        if i < UFIR_N
+            Y(:,i) = y;
+        elseif i == UFIR_N
+            Y(:,i) = y;
+            [xhat,F] = ufir_init(sys_d.a,sys_d.c,Y,UFIR_N);
+        else
+            [xhat,F] = ufir_update(sys_d.a,sys_d.c,y,xhat,F);
+        end
+
+        % Simulate actual system
+        state(:,1) = sys_d.a*state(:,2) + sys_d.b*input(i);
+        output(i,:) = sys_d.c*state(:,2) + sys_d.d*input(i);
+
+        % Save the new states in the old state
+        eststate(:,2) = eststate(:,1);
+        state(:,2) = state(:,1);
+
+        % Save so we can plot
+        estoutputhistory(i,:) = estoutput';
+        if i > UFIR_N
+            firoutputhistory(i,:) = xhat';
+        end
+    end
+
+    t = 0:T:(size(estoutputhistory,1)-1)/f;
+
+    %figure;
+    %[AX,H1,H2] = plotyy(t,output(:,1),t,output(:,2),'plot');
+    %set(get(AX(1),'Ylabel'),'String','cart position (m)');
+    %set(get(AX(2),'Ylabel'),'String','pendulum angle (radians)');
+    %title('Actual State - without lsim');
+
+    figure;
+    [AX,H1,H2] = plotyy(t,estoutputhistory(:,1),t,estoutputhistory(:,2),'plot');
+    set(get(AX(1),'Ylabel'),'String','cart position (m)');
+    set(get(AX(2),'Ylabel'),'String','pendulum angle (radians)');
+    title('4th Order State Estimates - Discrete');
+    print -dpng '4th_Order_State_Esimates_Discrete.png'
+
+    figure;
+    [AX,H1,H2] = plotyy(t,firoutputhistory(:,1),t,firoutputhistory(:,3),'plot');
+    set(get(AX(1),'Ylabel'),'String','cart position (m)');
+    set(get(AX(2),'Ylabel'),'String','pendulum angle (radians)');
+    title('4th Order UFIR State Estimates - Discrete');
+    print -dpng '4th_Order_UFIR_State_Estimates_Discrete.png'
+
+    % Plot the input, make sure it's not > Maxvoltage
+    figure;
+    plot(t,estoutputhistory(:,1),'-r',
+         t,estoutputhistory(:,2),'-b',
+         t,input,'-g');
+    title('4th Order Voltage Output');
+    print -dpng '4th_Order_Voltage_Output.png'
+end
---
+++ pendulum_wrong_run.m
@@ -0,0 +1,165 @@
+%
+% Control the inverted pendulum
+%
+
+close all;
+
+% load values from pendulum.m
+pendulum_wrong;
+
+% Load Octave packages we'll be using
+% Note: must do this below loading pendulum.m, since it clears everything
+pkg load sockets control;
+% load ctrlbox comm functions
+ctrlbox;
+
+% Let's run this controller.
+disp('');
+disp('');
+
+saveData = false;
+
+try
+    % Reset the board, then try connecting
+    disp('Set the cart in the middle, pendulum down and stationary.')
+    disp('Then click the "PROG" reset button on the FPGA board')
+    disp('Finally, rotate pendulum CCW to vertical.')
+    disp('');
+    input('Press Enter to continue.');
+
+    % Connection must first be established with the ctrlbox.
+    ctrlbox_init();
+    disp('Connected to ctrlbox');
+
+    % Send sample period
+    period = 1000000./f;
+    ctrlbox_send(0,0,period);
+    disp('Finished sending sample rate');
+
+    % Initial estimates are zero
+    eststate = zeros(size(A,1), 2);
+    input = 0;
+    estoutput = zeros(size(C,1),1);
+    control_output = 0;
+
+    % Plot output
+    estoutputhistory = zeros(1,size(C,1)+3);
+
+    % Only run for a certain time
+    cnt = 0;
+    maxTime = 20; % seconds
+    maxCnt = maxTime*f;
+
+    % Let's get rid of the first bit of data so that it doesn't instantly error
+    % saying the angle is too great
+    for n = 1:10
+        rdata = ctrlbox_recv();
+    end
+
+    while true
+    %while cnt < maxCnt
+        ++cnt;
+        % Read encoder values
+        %
+        % [long_pend_angle, short_pend_angle, motor_shaft_angle, knob_angle]
+        % Pendulum and motor shaft angles are 4096 counts/rev
+        rdata = ctrlbox_recv();
+        long_pend_angle = rdata(1)*2*pi/4096+pi;
+        short_pend_angle = rdata(2)*2*pi/4096;
+        motor_shaft_angle = rdata(3)*2*pi/4096;
+        motor_position = motor_shaft_angle*rd^2; % TODO why rd^2?
+        %motor_position = motor_shaft_angle*rd;
+        knob_angle = rdata(4);
+
+        % Checks for safety
+        if cnt == 0 && (
+            abs(motor_position) > pstart || ...
+            abs(short_pend_angle) > astart || ...
+            abs(long_pend_angle) > astart)
+           printf('Exiting. Invalid starting position or angle. ');
+           printf('Motor position: %f, Long Pendulum angle: %f, Short Pendulum angle: %f\n',
+               motor_position, long_pend_angle, short_pend_angle);
+           break
+        end
+
+        if abs(motor_position) > Maxpos || ...
+           %abs(short_pend_angle) > Maxangle || ...
+           abs(long_pend_angle) > Maxangle
+           printf('Exiting. Invalid position or angle. ');
+           printf('Motor position: %f, Long Pendulum angle: %f, Short Pendulum angle: %f\n',
+               motor_position, long_pend_angle, short_pend_angle);
+           break
+        end
+
+        % Set input
+        %r = knob_angle/1000;
+        r = 0.0;
+
+        % Estimation
+        y = [motor_position; long_pend_angle];
+        uvec = [control_output; y-estoutput];
+        eststate(:,1) = sys_est_only_d.a*eststate(:,2) + sys_est_only_d.b*uvec;
+        estoutput = sys_est_only_d.c*eststate(:,2) + sys_est_only_d.d*uvec;
+        eststate(:,2) = eststate(:,1);
+        control_output = r*Nbar - K*eststate(:,1);
+
+        % Test, use measurements in control even though we have a full-order observer
+        %control_output = r*Nbar - K*[y(1); eststate(2,1); y(2); eststate(4,1)];
+
+        % From book:
+        %control_output = r*Nbar - K*(eststate(:,1) - [1;0;0;0]*r);
+
+        % Write pwm values and enable motor
+        %
+        % Properly put between the +/- bounds of the PWM output.
+        %
+        % Scale properly, or at least in a way that appears to work.
+        % Experimentally determined 2000 to work, which is close to 32768/18
+        % (or 32768/20?) which came from past years' code, probably because 18
+        % or 20 is the max voltage, so we're converting so the DAC will output
+        % the desired voltage.
+        pwm = min(max(-control_output*32768/Maxvoltage,-32767),32767);
+        ctrlbox_send(pwm, 1, 0);
+
+        % Force matlab to check for interrupts and flush event queue
+        drawnow;
+        fflush(stdout);
+
+        if saveData
+            % Save data
+            printf('Long: %f, Short: %f, Motor: %f, Knob: %f, PWM: %f\n',
+                long_pend_angle, short_pend_angle, motor_shaft_angle, knob_angle, pwm);
+            estoutputhistory = [estoutputhistory;[estoutput' motor_position long_pend_angle short_pend_angle]];
+        end
+    end
+
+    % Send zero
+    pwm = 0;
+    ctrlbox_send(pwm, 1, 0);
+
+    drawnow;
+catch
+    % If something failed, display error
+    disp('Exiting...');
+    disp(lasterror.message);
+end
+
+if saveData
+    % Plot estimated states
+    t = 0:T:(size(estoutputhistory,1)-1)/f;
+    figure;
+    [AX,H1,H2] = plotyy(t,estoutputhistory(:,1),t,estoutputhistory(:,2),'plot');
+    set(get(AX(1),'Ylabel'),'String','cart position (m)');
+    set(get(AX(2),'Ylabel'),'String','pendulum angle (radians)');
+    title('4th Order Live State Estimates');
+    print -dpng "4th_Order_Live_State_Estimates.png"
+    figure;
+    [AX,H1,H2] = plotyy(t,estoutputhistory(:,3),t,estoutputhistory(:,4),'plot');
+    set(get(AX(1),'Ylabel'),'String','measured cart position (m)');
+    set(get(AX(2),'Ylabel'),'String','measured pendulum angle (radians)');
+    title('4th Order Live Measured values');
+    print -dpng "4th_Order_Live_Measured_Values.png"
+end
+
+% disable motor and disconnect
+ctrlbox_shutdown();
